0fcc0693eec7773d06d6b8d23512cbf79d6e6a02
root <root@localhost>
2017-10-16 10:58:15 +0900

    1710161058ConflictTestMaster

--- WelcomeToHellWorld/src/HellWorld.java
+++ WelcomeToHellWorld/src/HellWorld.java
@@ -4,7 +4,8 @@ public class HellWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HellWorld");
-		System.out.println("HellWorld for hotfix");
+		System.out.println("HellWorld for hotfix xx");
+		System.out.println("HellWorld for hotfix one");
 	}
 
 }
