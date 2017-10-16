f28a9dd0ec5bb6f19ddae08156b7cc5243bce594
root <root@localhost>
2017-10-16 10:52:10 +0900

    1710161052HotfixTest

--- WelcomeToHellWorld/src/HellWorld.java
+++ WelcomeToHellWorld/src/HellWorld.java
@@ -4,7 +4,7 @@ public class HellWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HellWorld");
-
+		System.out.println("HellWorld for hotfix");
 	}
 
 }
